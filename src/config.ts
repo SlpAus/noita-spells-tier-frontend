2811db52b4407f2dae012f10d1c9ab3fd0e1feaf
11QiuY <2596426263@qq.com>
2025-02-27 14:19:42 +0800

    can deploy to cloud

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-const BACKEN_URL = "http://localhost:4444";
-const FRONTEND_URL = "http://localhost:3000";
+const BACKEN_URL = process.env.REACT_APP_BACKEND_URL || "http://localhost:8080";
+const FRONTEND_URL = process.env.REACT_APP_FRONTEND_URL || "http://localhost:3000";
 
 export { BACKEN_URL, FRONTEND_URL };
