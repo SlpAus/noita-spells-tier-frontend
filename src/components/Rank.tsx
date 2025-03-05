4c841f42e0b2eb091c66ee3052c349e0aa4dae64
11QiuY <2596426263@qq.com>
2025-03-05 13:35:07 +0800

    change rank

--- src/components/Rank.tsx
+++ src/components/Rank.tsx
@@ -1,16 +1,12 @@
-//// filepath: /home/qiuy/projects/ys_ranking/ys-voting-frontend/src/components/Rank.tsx
 import { ranking } from "../types/ranking";
 import Box from "./UI/Box";
 import { useState } from "react";
 
 export default function Rank(props: { rank: ranking[], title: string, onRefresh: () => void }) {
     const [isRefreshing, setIsRefreshing] = useState(false);
+    const [isExpanded, setIsExpanded] = useState(false);
     const [sortOrder, setSortOrder] = useState<"asc" | "desc">("desc");
     const [sortBy, setSortBy] = useState<"total" | "score" | "winpercent">("score");
-    const PAGE_SIZE = 100;
-    const [currentPage, setCurrentPage] = useState(1);
-    const [jumpPage, setJumpPage] = useState(""); // 输入跳转页码
-
     const toggleSortOrder = (criteria: "score" | "winpercent" | "total") => {
         if (sortBy === criteria) {
             setSortOrder(prevOrder => (prevOrder === "asc" ? "desc" : "asc"));
@@ -19,7 +15,6 @@ export default function Rank(props: { rank: ranking[], title: string, onRefresh:
             setSortOrder("desc");
         }
     };
-
     const sortedRank = [...props.rank].sort((a, b) => {
         if (sortBy === "score") {
             return sortOrder === "asc" ? a.score - b.score : b.score - a.score;
@@ -30,12 +25,16 @@ export default function Rank(props: { rank: ranking[], title: string, onRefresh:
         }
     });
 
-    const totalPages = Math.ceil(sortedRank.length / PAGE_SIZE);
+    const NotExpandSize = 30;
 
     const handleRefresh = async () => {
         setIsRefreshing(true);
         await props.onRefresh();
         setIsRefreshing(false);
+    };
+
+    const toggleExpand = () => {
+        setIsExpanded(!isExpanded);
     };
 
     const getBackgroundColor = (index: number) => {
@@ -63,8 +62,9 @@ export default function Rank(props: { rank: ranking[], title: string, onRefresh:
     return (
         <Box>
             <div className="flex justify-between items-center px-10 py-5">
-                <div className="flex-1 text-center">
-                    <h2 className="text-4xl font-bold">{props.title}</h2>
+                <div className="flex-1 text-center hover:bg-gray-200 transition-colors duration-300">
+                    <button onClick={toggleExpand}
+                        className="text-4xl font-bold">{props.title}</button>
                 </div>
                 <button
                     onClick={handleRefresh}
@@ -98,72 +98,30 @@ export default function Rank(props: { rank: ranking[], title: string, onRefresh:
                 </button>
             </div>
             <div className="flex flex-col rounded-xl">
-                {sortedRank.slice((currentPage - 1) * PAGE_SIZE, currentPage * PAGE_SIZE).map((item, index) => (
-                    <div key={(currentPage - 1) * PAGE_SIZE + index} className={`flex justify-between px-5 py-0.5 ${getBackgroundColor((currentPage - 1) * PAGE_SIZE + index)}`}>
-                        <p className="text-lg text-center font-semibold w-1/6">{getCupLevel((currentPage - 1) * PAGE_SIZE + index)}</p>
-                        <p className="text-lg text-center w-1/6">{item.rank}</p>
-                        <p className="text-lg text-center w-1/6">{item.name}</p>
-                        <p className="text-lg text-center w-1/6">{item.score.toFixed(1)}</p>
-                        <p className="text-lg text-center w-1/6">{(item.winpercent * 100).toFixed(1)}%</p>
-                        <p className="text-lg text-center w-1/6">{(item.totals).toFixed(1)}</p>
-                    </div>
-                ))}
+                {sortedRank.map((item, index) => {
+                    if (!isExpanded && index >= NotExpandSize) {
+                        return null;
+                    }
+                    return (
+                        <div key={index} className={` flex justify-between px-5 py-0.5 ${getBackgroundColor(index)}`}>
+                            <p className="text-lg text-center font-semibold w-1/6">{getCupLevel(index)}</p>
+                            <p className="text-lg text-center w-1/6">{item.rank}</p>
+                            <p className="text-lg text-center w-1/6">{item.name}</p>
+                            <p className="text-lg text-center w-1/6">{item.score.toFixed(1)}</p>
+                            <p className="text-lg text-center w-1/6">{(item.winpercent * 100).toFixed(1)}%</p>
+                            <p className="text-lg text-center w-1/6">{(item.totals).toFixed(1)}</p>
+                        </div>
+                    );
+                })}
             </div>
-            <div className="w-full flex flex-col items-center space-y-2 px-10 py-2 border-t-2 border-gray-200">
-                <div className="flex justify-center items-center space-x-4">
-                    <button
-                        onClick={() => setCurrentPage(1)}
-                        disabled={currentPage === 1}
-                        className="px-4 py-1 bg-gray-300 rounded disabled:opacity-50 hover:bg-gray-700 transition-colors duration-300 hover:text-white"
-                    >
-                        首页
-                    </button>
-                    <button
-                        onClick={() => setCurrentPage(prev => Math.max(prev - 1, 1))}
-                        disabled={currentPage === 1}
-                        className="px-4 py-1 bg-gray-300 rounded disabled:opacity-50 hover:bg-gray-700 transition-colors duration-300 hover:text-white"
-                    >
-                        上一页
-                    </button>
-                    <span className="text-xl font-bold">
-                        第 {currentPage} 页 / 共 {totalPages} 页
-                    </span>
-                    <button
-                        onClick={() => setCurrentPage(prev => Math.min(prev + 1, totalPages))}
-                        disabled={currentPage === totalPages}
-                        className="px-4 py-1 bg-gray-300 rounded disabled:opacity-50 hover:bg-gray-700 transition-colors duration-300 hover:text-white"
-                    >
-                        下一页
-                    </button>
-                    <button
-                        onClick={() => setCurrentPage(totalPages)}
-                        disabled={currentPage === totalPages}
-                        className="px-4 py-1 bg-gray-300 rounded disabled:opacity-50 hover:bg-gray-700 transition-colors duration-300 hover:text-white"
-                    >
-                        尾页
-                    </button>
-                </div>
-                <div className="flex items-center space-x-2">
-                    <input
-                        type="number"
-                        value={jumpPage}
-                        onChange={(e) => setJumpPage(e.target.value)}
-                        placeholder="请输入页码"
-                        className="p-1 border rounded text-center"
-                    />
-                    <button
-                        onClick={() => {
-                            const targetPage = Number(jumpPage);
-                            if (targetPage >= 1 && targetPage <= totalPages) {
-                                setCurrentPage(targetPage);
-                            }
-                        }}
-                        className="px-3 py-1 bg-blue-400 text-white rounded hover:bg-blue-600 transition-colors duration-300"
-                    >
-                        确定
-                    </button>
-                </div>
-            </div>
+            <button
+                onClick={toggleExpand}
+                className="w-full flex justify-center items-center px-10 py-1 border-t-2 border-gray-200 hover:bg-gray-100 transition-colors duration-300"
+            >
+                <p className="text-xl font-bold">
+                    {isExpanded ? "▲ 收起" : "▼ 展开"}
+                </p>
+            </button>
         </Box>
     );
 }
